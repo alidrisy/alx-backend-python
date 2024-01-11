b6fae7ac95028b2af7a886887ce73c682d842219
alidrisy <a.alidrisy6@gmail.com>
2024-01-11 20:26:31 +0300

    Fix syntax order erorr

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Function to takes a string k and an int OR float v as
     arguments and returns a tuple. """
     return (k, v * v)
