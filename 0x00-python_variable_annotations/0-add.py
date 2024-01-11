745501c7c37b15ab2c09ab8433a05f80bc999309
alidrisy <a.alidrisy6@gmail.com>
2024-01-11 19:31:18 +0300

    1. Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Model to add tow float numbers """
 
+
 def add(a: float, b: float) -> float:
     """ Type-annotated function add tow float numbers """
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Model to concatenated string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function to concatenated tow strings """
+    return str1 + str2
