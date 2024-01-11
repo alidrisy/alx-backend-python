6c5fe2cd946e338e1c7e75afe1775ef915b8dc7e
alidrisy <a.alidrisy6@gmail.com>
2024-01-11 20:51:11 +0300

    using lambda instead of function

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,12 +3,7 @@
 from typing import Callable
 
 
-def sqrt(num: float) -> float:
-    """ Function that return the squer of float """
-    return num * num
-
-
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ Function that takes a float multiplier as argument and returns
     a function that multiplies a float by multiplier."""
-    return sqrt
+    return lambda x: x * multiplier
