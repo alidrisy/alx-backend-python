1b971a8359f652c138ba50fa58a0bfec1e6d2b3d
alidrisy <a.alidrisy6@gmail.com>
2024-01-11 20:15:15 +0300

    7. Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,6 +7,6 @@ def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ Function which takes a list mxd_list of floats and integers
     as argument and returns their sum as a float. """
     x: float = 0.0
-    for i in mxd_list:
+    for i in mxd_lst:
         x += i
     return x
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Model to take string and float/int and return tuple """
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """ Function to takes a string k and an int OR float v as
+    arguments and returns a tuple. """
+    return (k, v * v)
