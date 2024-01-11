d8e509c61c761743d9518e9f2cfd128e2b1946eb
alidrisy <a.alidrisy6@gmail.com>
2024-01-11 19:56:48 +0300

    using typing.List instead of list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Model to return the sum of float list """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Function which takes a list input_list of floats as argument
     and returns their sum as a float. """
     x: float = 0.0
