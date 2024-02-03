eda0ea44b1744a5504499f120ef80b83cb5cb3bc
alidrisy <a.alidrisy6@gmail.com>
2024-02-03 19:15:50 +0300

    5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 from parameterized import parameterized
 from client import GithubOrgClient
 from typing import Callable, Mapping
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import unittest
 
 
@@ -23,6 +23,20 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, resp)
         resp = git_hub_org.org
         get_json.assert_called_once()
+
+    @parameterized.expand([
+        ("google", {"repos_url": "https://api.github.com/orgs/google"}),
+        ("abc", {"repos_url": "https://api.github.com/orgs/abc"})
+    ])
+    def test_public_repos_url(self, org_name: str, result: Mapping):
+        """Test that the result of _public_repos_url is the expected
+        one based on the mocked payload."""
+        with patch.object(GithubOrgClient, "org",
+                          new_callable=PropertyMock) as org:
+            org.return_value = result
+            git_hub_org = GithubOrgClient(org_name)
+            resp = git_hub_org._public_repos_url
+            self.assertEqual(result["repos_url"], resp)
 
 
 if __name__ == "__main__":
