367f55ae70dcab09f3af25785cdf4034d473febf
alidrisy <a.alidrisy6@gmail.com>
2024-02-03 20:34:16 +0300

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ Model for client model methods """
 from parameterized import parameterized
 from client import GithubOrgClient
-from typing import Callable, Mapping
+from typing import Callable, Mapping, Sequence
 from unittest.mock import patch, PropertyMock
 import unittest
 
@@ -37,6 +37,29 @@ class TestGithubOrgClient(unittest.TestCase):
             git_hub_org = GithubOrgClient(org_name)
             resp = git_hub_org._public_repos_url
             self.assertEqual(result["repos_url"], resp)
+
+    @parameterized.expand([
+        ("https://api.github.com/orgs/alx",
+         [{"repos_url": "https://api.github.com/orgs/alx", "name": "alx"}],
+          ["alx"]),
+        ("https://api.github.com/orgs/abc",
+         [{"repos_url": "https://api.github.com/orgs/abc", "name": "abc"}],
+          ["abc"])
+    ])
+    @patch("client.get_json")
+    def test_public_repos(self, repos_url: str, repos: Sequence,
+                          result: Sequence, get_json: Callable):
+        """test that GithubOrgClient.public_repos returns the correct value."""
+        with patch.object(GithubOrgClient, "_public_repos_url",
+                          new_callable=PropertyMock) as _public_repos_url:
+            _public_repos_url.return_value = repos_url
+            git_hub_org = GithubOrgClient(result[0])
+            get_json.return_value = repos
+            resp = git_hub_org.public_repos()
+            self.assertEqual(resp, result)
+            git_hub_org.public_repos()
+            _public_repos_url.assert_called_once()
+            get_json.assert_called_once()
 
 
 if __name__ == "__main__":
