d8918667db09c8ff7088fceaf2d7dbe168dc69c3
Abdulrahman Alidrisy <a.alidrisy6@gmail.com>
2025-05-18 11:44:30 +0300

    feat: Implement batch processing of user data using generators

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import seed
+from itertools import islice
+
+
+def stream_users_in_batches(batch_size):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    batches = stream_users_in_batches(batch_size)
+    for batch in batches:
+        for user in islice(batch, 6):
+            if user.get("age", 0) > 25:
+                print(user)
