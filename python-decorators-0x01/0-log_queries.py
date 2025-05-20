480a0707fa81253d479941016bac22701fe7d2d1
alidrisy <a.alidrisy6@gmail.com>
2025-05-20 22:10:16 +0300

    feat: Enhance logging functionality in log_queries decorator and add fetch_all_users function

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,19 +1,41 @@
+#!/usr/bin/python3
+"""
+This module contains a decorator `log_queries` for logging function arguments and a function `fetch_all_users` to fetch user data from a SQLite database.
+"""
 import sqlite3
 import functools
+from datetime import datetime
 
-#### decorator to lof SQL queries
 def log_queries(func):
+    """
+    A decorator that logs the arguments passed to the decorated function.
+
+    Args:
+        func (callable): The function to be decorated.
+
+    Returns:
+        callable: The wrapped function with logging functionality.
+    """
     def wrapper(*args, **kwargs):
         if args:
-            print(args)
-        if len(kwargs.keys()) > 0:
-            print(kwargs)
+            print("Positional arguments:", args, "at", datetime.now().isoformat())
+        if kwargs:
+            print("Keyword arguments:", kwargs, "at", datetime.now().isoformat())
         return func(*args, **kwargs)
     return wrapper
 
 
 @log_queries
 def fetch_all_users(query):
+    """
+    Fetches all users from the database based on the provided SQL query.
+
+    Args:
+        query (str): The SQL query to execute.
+
+    Returns:
+        list: A list of tuples containing the query results.
+    """
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(query)
@@ -21,6 +43,5 @@ def fetch_all_users(query):
     conn.close()
     return results
 
-#### fetch users while logging the query
+# Example usage
 users = fetch_all_users(query="SELECT * FROM users")
-print(users)
