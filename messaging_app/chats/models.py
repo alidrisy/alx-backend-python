daf105f7baa324d350472720b6b78d2c3dec96c7
alidrisy <a.alidrisy6@gmail.com>
2025-05-31 21:38:02 +0300

    1. Define data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+
+# Extend Django’s built-in user model
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
+
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages"
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.username} @ {self.timestamp}: {self.content[:30]}"
