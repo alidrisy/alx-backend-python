bd94f4d86ac69e1cc4464459a81814fe57f7b63e
alidrisy <a.alidrisy6@gmail.com>
2025-05-31 21:43:45 +0300

    feat: Enhance User model with additional fields for first name, last name, email, and password

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -5,6 +5,10 @@ import uuid
 
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
