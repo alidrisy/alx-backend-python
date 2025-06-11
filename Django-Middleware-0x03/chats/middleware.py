3dae8f37d73ab55b227c4ba2af0f9722dcb2b44c
Abdulrahman Alidrisy <a.alidrisy6@gmail.com>
2025-06-11 14:04:22 +0300

    feat: implement request logging with file output for user requests

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,12 +1,26 @@
+# chats/middleware.py
+
+import logging
 from datetime import datetime
 
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.logger = logging.getLogger(__name__)
+        handler = logging.FileHandler("requests.log")
+        formatter = logging.Formatter("%(message)s")
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
 
     def __call__(self, request):
         user = request.user
-        print(f"{datetime.now()} - User: {user} - Path: {request.path}")
+        path = request.path
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        log_message = f"{timestamp} - User: {user} - Path: {path}"
+        self.logger.info(log_message)
+
         response = self.get_response(request)
         return response
